e9aa41fa6a80ca05a431739aab5fb20177b3661a
Sergey Peshkov <zloi.g1rz@gmail.com>
2022-01-10 17:55:12 +0800

    Добавил основные drag события

--- scripts.js
+++ scripts.js
@@ -29,9 +29,56 @@ function init() {
 
     puzzleItems.forEach(image => {
         $puzzleHash.append(image)
-    })
+
+        
+    });
 }
 
+let dragged;
+document.addEventListener('dragstart', function(event) {
+    console.log('dragstart');
+     // store a ref. on the dragged elem
+     dragged = event.target;
+     // make it half transparent
+     event.target.style.opacity = .5;
+});
+
+document.addEventListener('dragend', function(event) {
+    console.log('dragend');
+    event.target.style.opacity = "";
+});
+
+document.addEventListener('dragover', function(event) {
+    console.log('dragover');
+    if ( event.target.className == "puzzle-cell" ) {
+        event.target.style.background = "purple";
+    }
+});
+
+document.addEventListener('dragenter', function(event) {
+    console.log('dragenter');
+    if ( event.target.className == "puzzle-cell" ) {
+        event.target.style.background = "red";
+    }
+});
+
+document.addEventListener('dragleave', function(event) {
+    console.log('dragleave');
+    if ( event.target.className == "puzzle-cell" ) {
+        event.target.style.background = "#fff";
+    }
+});
+
+document.addEventListener("drop", function( event ) {
+    event.preventDefault();
+    // move dragged elem to the selected drop target
+    if ( event.target.className == "puzzle-cell" ) {
+        event.target.style.background = "";
+        dragged.parentNode.remove( dragged );
+        event.target.append( dragged );
+    }
+  
+}, false);
 
 // нарезает исходное изображение на массив объектов картинок для пазла, 
 // где src - часть изображения, x и y - координаты пазла
